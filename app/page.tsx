dab64dd7bf97bdb8da6cb0be298ba61b45fdb700
imodoiepale <ijepale@gmail.com>
2024-04-19 09:51:38 +0300

    style

--- app/page.tsx
+++ app/page.tsx
@@ -551,7 +551,7 @@ const handleComplete = async () => {
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                    <h3 className="text-lg uppercase leading-6 font-bold text-gray-900" id="modal-title">
                       Appointment Details for {selectedAppointment.client_name}
                     </h3>
 
